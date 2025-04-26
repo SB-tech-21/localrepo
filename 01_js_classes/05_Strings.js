4f019d0e4ac6073f02611448419426beeebe214d
SB-tech-21 <surjithbaig09@gmail.com>
2025-04-26 18:02:24 +0000

    string methods is discussed

---
+++ 01_js_classes/05_Strings.js
@@ -0,0 +1,16 @@
+const name='Surjith'
+const repocount = 50
+console.log(name + repocount);
+
+//String Interpretation - Helps us to inject values this is similar to string formating in python where we use '{}' to assign values
+
+console.log(`Hi, My name is ${name} and my repocount value is ${repocount}!`);
+
+const gameName = new String('SurjithBaig')// here this new keyword represents the creation of object
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName[3]);
+console.log(gameName.charAt(6));
+
+
