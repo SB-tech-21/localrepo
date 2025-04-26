a719f9f8ee1f8acc57657b65626c74d540d797d2
SB-tech-21 <surjithbaig09@gmail.com>
2025-04-26 08:20:24 +0000

    deleted unwanted files and added 2 classes files

---
+++ 01_js_classes/02_datatypes and EMCA.js
@@ -0,0 +1,12 @@
+// DataTypes present in JS
+
+/*
+Number-1313
+String -"Surjith"
+Null - it is an object along with data type
+undefined - its data type is undefined itself
+console.log(typeof "null")
+*/
+
+let normal= null
+console.log(typeof(normal))
