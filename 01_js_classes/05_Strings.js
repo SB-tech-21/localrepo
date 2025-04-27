678ab8082589ad756c4e41e80d249c1edd522753
SB-tech-21 <surjithbaig09@gmail.com>
2025-04-27 07:27:09 +0000

    completed the strings

--- 01_js_classes/05_Strings.js
+++ 01_js_classes/05_Strings.js
@@ -7,10 +7,29 @@ console.log(name + repocount);
 console.log(`Hi, My name is ${name} and my repocount value is ${repocount}!`);
 
 const gameName = new String('SurjithBaig')// here this new keyword represents the creation of object
-console.log(gameName.__proto__);
-console.log(gameName.toUpperCase());
-console.log(gameName.length);
-console.log(gameName[3]);
-console.log(gameName.charAt(6));
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName[3]);
+// console.log(gameName.charAt(6));
+// console.log(gameName.indexOf('t'));
+// console.log(gameName.substring(0,4));// doesnt take negative values as input
+// anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+fatherName='      Rahim Baig         '
+// console.log(fatherName.trim());// removes the front and back whitespaces
+
+// console.log(fatherName.replace('Rahim','Irfan'));// replace method lets u to choose the character that u want to replace with.
+// console.log(fatherName);
+
+motherName='Fhameetha-Begum'
+console.log(motherName.includes('Fhameetha'))
+
+console.log(motherName.split('-'));
+
+
+
+
 
 
